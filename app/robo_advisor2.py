9f1c21fb76576b6b82b34864246dc500437d8f7c
ecemarc <ece.marcelli@stern.nyu.edu>
2020-06-13 22:15:15 -0400

    Printing the information requested!

--- app/robo_advisor2.py
+++ app/robo_advisor2.py
@@ -1,40 +1,34 @@
 # my-secure-project/my_script.py
-import datetime
 import re
-import csv
-import requests
+import json
 from dotenv import load_dotenv
 import os
-import json  # json is similar to dict or lists we have been using
+import csv
 import requests
 
+import datetime
 
-load_dotenv()  # NEEDS TO GO BEFORE ACCESSING KEY
+
+import datetime
+now = datetime.datetime.now()
+# print(os.getenv("ALPHAVANTAGE_API_KEY"))  # > None
+
+load_dotenv()  # > loads contents of the .env file into the script's environment
+
+
 api_key = os.getenv("ALPHAVANTAGE_API_KEY")
 
 
-now = datetime.datetime.now()
+# print(api_key)
 
-
-#
-# INFO INPUTS
-#
-#
-
-# use request package to get a response, then convert the string to a dictionary or a list so you can work on it.
-# check apis.md in prof. repo
-# ENVIRON.GET DID NOT WOR, SWITCHED TO GETENV
-
+# REFERENCED https://github.com/prof-rossetti/intro-to-python/blob/master/exercises/api-client/solution.py
 
 def getting_url(symbol_input):
     symbol_input = selected_stock
     request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol_input}&apikey={api_key}"
-    return requests.get(request_url)
-# print(type(response))  # class
-# print(response.status_code)  # we got 200, sso good to go
-# print(response.text)  # string-need to import json module
+    return request_url
 
-# followed Guided Screen Cast by Prof Rosetti and then modified.
+# followed Guided Screen Cast
 
 
 def process_ticker(symbol):
@@ -87,6 +81,7 @@ def transform_response(parsed_response):
 # print(response.status_code)  # we got 200, sso good to go
 # print(response.text)  # string-need to import json module
 
+
 if __name__ == "__main__":  # revisited rock-paper and input module
     while True:
         selected_stock = input(
@@ -102,8 +97,7 @@ if __name__ == "__main__":  # revisited rock-paper and input module
         else:
             break
 
-# parse use the json module called jason.loads to change response.text to dictionary
-# followed Professor Rosetti's guided video
+    # followed Professor Rosetti's guided video
     parsed_response = process_ticker(selected_stock)
     last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
     row = transform_response(parsed_response)
@@ -132,33 +126,12 @@ if __name__ == "__main__":  # revisited rock-paper and input module
 
     recent_highest = max(recent_highs)  # creating a list and
     recent_lowest = min(recent_lows)
-    #
-    #
-    #
-    # csv_file_path = "data/prices.csv"  # a relative filepath
 
+    # csv_file_path = "data/prices.csv"  # a relative filepath
     csv_file_path = os.path.join(os.path.dirname(
         __file__), "..", "data", "prices.csv")
-
-    csv_column_headers = ["timestamp", "open",
-                          "high", "low", "close", "volume"]
-
     # timestamp, open, high, low, close, volume
 
-    with open(csv_file_path, "w") as csv_file:  # "w" means "open the file for writing"
-        writer = csv.DictWriter(csv_file, fieldnames=csv_column_headers)
-        writer.writeheader()  # uses fieldnames set above
-        for date in dates:
-            minute_prices = time_series[date]
-
-            writer.writerow({
-                "timestamp": date,
-                "open": minute_prices["1. open"],
-                "high": minute_prices["2. high"],
-                "low": minute_prices["3. low"],
-                "close": minute_prices["4. close"],
-                "volume": minute_prices["5. volume"]
-            })
     print("*****************************************************")
     # referenced geeksforgeeks upper-lower input applications
     print("SELECTED SYMBOL:" + selected_stock.upper())
